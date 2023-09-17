037eaf2c1e6821958e7c2de6bd097f86ed6eeb53
sefensi <sefensi_reload@mail.ru>
2023-09-17 19:57:40 +0500

    Counter: add days

--- frontend/src/m.time.js
+++ frontend/src/m.time.js
@@ -2,6 +2,7 @@ export class Time {
 
     constructor() {
         this.time = {
+            days: 0,
             hours: 0,
             minutes: 0,
             seconds: 0,
@@ -12,33 +13,39 @@ export class Time {
     sumOfTimes(timeObj) {
         for (const key in this.time) {
             switch (key) {
+                
                 case 'hours':
                     this.time[key] += timeObj[key]
+                    if (this.time[key] > 23) {
+                        this.time.days += 1
+                        this.time[key] -= 24
+                    
+                    }
                     break;
 
                 case 'minutes':
                     this.time[key] += timeObj[key]
-                    if (this.time[key] > 59){
+                    if (this.time[key] > 59) {
                         this.time.hours += 1
                         this.time[key] -= 60
                     }
                     break;
-                    
+
                 case 'seconds':
                     this.time[key] += timeObj[key]
-                    if (this.time[key] > 59){
+                    if (this.time[key] > 59) {
                         this.time.minutes += 1
                         this.time[key] -= 60
                     }
                     break;
                 case 'miliseconds':
                     this.time[key] += timeObj[key]
-                    if (this.time[key] > 99){
+                    if (this.time[key] > 99) {
                         this.time.seconds += 1
                         this.time[key] -= 100
                     }
                     break;
-            
+
             }
             // if ((this.time[key] + timeObj[key]) > 59) {
             //     this.time['seconds'] += timeObj[key] - 60 
@@ -51,6 +58,6 @@ export class Time {
     }
 
     visual() {
-        document.getElementById("clock2").textContent = `${this.time.hours}:${this.time.minutes}:${this.time.seconds}`
+        document.getElementById("clock2").textContent = `${this.time.days}:${this.time.hours}:${this.time.minutes}:${this.time.seconds}`
     }
 }
