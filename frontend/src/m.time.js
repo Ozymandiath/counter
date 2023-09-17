f804581a032431a98b2edff2ad1fb73c3a9be415
sefensi <sefensi_reload@mail.ru>
2023-09-17 18:44:36 +0500

    Counter: fixing the values

--- frontend/src/m.time.js
+++ frontend/src/m.time.js
@@ -1,18 +1,51 @@
 export class Time {
 
-    constructor(){
+    constructor() {
         this.time = {
             hours: 0,
             minutes: 0,
-            seconds: 0
+            seconds: 0,
+            miliseconds: 0
         }
     }
-    
+
     sumOfTimes(timeObj) {
         for (const key in this.time) {
-            console.log(this.time[key])
-            console.log(timeObj[key])
-            this.time[key] += timeObj[key]
+            switch (key) {
+                case 'hours':
+                    this.time[key] += timeObj[key]
+                    break;
+
+                case 'minutes':
+                    this.time[key] += timeObj[key]
+                    if (this.time[key] > 59){
+                        this.time.hours += 1
+                        this.time[key] -= 60
+                    }
+                    break;
+                    
+                case 'seconds':
+                    this.time[key] += timeObj[key]
+                    if (this.time[key] > 59){
+                        this.time.minutes += 1
+                        this.time[key] -= 60
+                    }
+                    break;
+                case 'miliseconds':
+                    this.time[key] += timeObj[key]
+                    if (this.time[key] > 99){
+                        this.time.seconds += 1
+                        this.time[key] -= 100
+                    }
+                    break;
+            
+            }
+            // if ((this.time[key] + timeObj[key]) > 59) {
+            //     this.time['seconds'] += timeObj[key] - 60 
+            //     this.time['minutes'] += 1
+            // }
+            // this.time[key] += timeObj[key]
+
         }
         this.visual()
     }
